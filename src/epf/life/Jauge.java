7ca3349d24c6d8be8a2b21e479741e863be68342
Utilisateur <Utilisateur@DESKTOP-KCACBUF.home>
2021-03-10 14:47:04 +0100

    Classe Jauge finie

--- src/epf/life/Jauge.java
+++ src/epf/life/Jauge.java
@@ -11,5 +11,54 @@ package epf.life;
  * @author Utilisateur
  */
 public class Jauge {
+   
+    
+// Attributs --------------------------------------
+    
+    
+    private int Valeur;
+    private int ValeurMax;
+    private String Nom;
+    
+    
+// --------------------------------------
+    
+    
+    
+    Jauge(int valeur, int valeurmax, String nom) {
+        this.Valeur = valeur;
+        this.ValeurMax = valeurmax;
+        this.Nom = nom;
+        
+    }
+    
+    public int LireValeur() {
+        return Valeur;
+    }
+    
+    public int LireValeurMax() {
+        return ValeurMax;
+    }
+    
+    public String LireNom() {
+        return Nom;
+    }
+    
+    public void affecterValeur(int valeurAajouter) {
+        Valeur += valeurAajouter;
+        
+        if(Valeur>100) {
+            Valeur = 100;
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
