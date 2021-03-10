d3c0a17b012300127c4fe1a68fd791857cca8cab
augus <augus@DESKTOP-QK32DV4>
2021-03-10 15:26:00 +0100

    Class randomEvent modification

--- src/epf/life/RandomEvent.java
+++ src/epf/life/RandomEvent.java
@@ -24,18 +24,18 @@ public class RandomEvent {
     }
     
     public void initialiserTableauDeChoix(int jv1, int jv2, int jv3,int jv4, int js1, int js2, int js3, int js4, int je1, int je2, int je3, int je4) {
-        TableauDeChoix[1][1]=jv1;
-        TableauDeChoix[1][2]=jv2;
-        TableauDeChoix[1][3]=jv3;
-        TableauDeChoix[1][4]=jv4;
-        TableauDeChoix[2][1]=js1;
-        TableauDeChoix[2][2]=js2;
-        TableauDeChoix[2][3]=js3;
-        TableauDeChoix[2][4]=js4;
-        TableauDeChoix[3][1]=je1;
-        TableauDeChoix[3][2]=je2;
-        TableauDeChoix[3][3]=je3;
-        TableauDeChoix[3][4]=je4;
+        TableauDeChoix[0][0]=jv1;
+        TableauDeChoix[0][1]=jv2;
+        TableauDeChoix[0][2]=jv3;
+        TableauDeChoix[0][3]=jv4;
+        TableauDeChoix[1][0]=js1;
+        TableauDeChoix[1][1]=js2;
+        TableauDeChoix[1][2]=js3;
+        TableauDeChoix[1][3]=js4;
+        TableauDeChoix[2][0]=je1;
+        TableauDeChoix[2][1]=je2;
+        TableauDeChoix[2][2]=je3;
+        TableauDeChoix[2][3]=je4;
     }
     
     public String lireNom() {
