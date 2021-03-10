e8f02213a5c5de4aabd12ec3e7cac1cc659ff31e
augus <augus@DESKTOP-QK32DV4>
2021-03-10 15:56:47 +0100

    Class ListeEvent

--- src/epf/life/ListeEvent.java
+++ src/epf/life/ListeEvent.java
@@ -11,4 +11,48 @@ package epf.life;
  */
 public class ListeEvent {
     
+    private DailyEvent ListeEventMatin[];
+    private RandomEvent ListeEventMatinRandom[];
+    private DailyEvent ListeEventMidi[];
+    private RandomEvent ListeEventMidiRandom[];
+    private DailyEvent ListeEventApresMidi[];
+    private RandomEvent ListeEventApresMidiRandom[];
+    private DailyEvent ListeEventSoir[];
+    private RandomEvent ListeEventSoirRandom[];
+    
+    public ListeEvent() {
+        
+    }
+    
+    public DailyEvent recupererEventMatin(int num) {
+        return ListeEventMatin[num];
+    }
+    
+    public RandomEvent recupererEventMatinRandom(int num) {
+        return ListeEventMatinRandom[num];
+    }
+    
+    public DailyEvent recupererEventMidi(int num) {
+        return ListeEventMidi[num];
+    }
+    
+    public RandomEvent recupererEventMidiRandom(int num) {
+        return ListeEventMidiRandom[num];
+    }
+    
+    public DailyEvent recupererEventApresMidi(int num) {
+        return ListeEventApresMidi[num];
+    }
+    
+    public RandomEvent recupererEventApresMidiRandom(int num) {
+        return ListeEventApresMidiRandom[num];
+    }
+    
+    public DailyEvent recupererEventSoir(int num) {
+        return ListeEventSoir[num];
+    }
+    
+    public RandomEvent recupererSoirRandom(int num) {
+        return ListeEventSoirRandom[num];
+    }
 }
